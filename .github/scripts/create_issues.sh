ac576054d7b26cafce0734c943af610a3cd6cf8d
Marcel Fonseca <marcel.resende@yahoo.com.br>
2024-08-01 14:16:22 -0300

    Update create_issues.sh

--- .github/scripts/create_issues.sh
+++ .github/scripts/create_issues.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-
 results=$1
-
 echo "${results}" | jq -c '.[]' | while read -r result; do
   title=$(echo "${result}" | jq -r '.title')
   severity=$(echo "${result}" | jq -r '.severity')
   correction=$(echo "${result}" | jq -r '.correction')
   body="**Severity:** ${severity}\n\n**Correction:** ${correction}"
-  
   curl -X POST -H "Authorization: token $GH_TOKEN" \
        -H "Accept: application/vnd.github.v3+json" \
        https://api.github.com/repos/${GITHUB_REPOSITORY}/issues \
