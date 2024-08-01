5389c006de53b941f9dbdc5b374c7efc33904fab
Marcel Fonseca <marcel.resende@yahoo.com.br>
2024-08-01 14:23:58 -0300

    Update create_issues.sh

--- .github/scripts/create_issues.sh
+++ .github/scripts/create_issues.sh
@@ -8,10 +8,9 @@ results=$(echo "$results" | sed "s/'/\"/g")
 
 # Processa o JSON e cria issues no GitHub
 echo "${results}" | jq -c '.[]' | while read -r result; do
-    title=$(echo "${result}" | jq -r '.title')
-    severity=$(echo "${result}" | jq -r '.severity')
     correction=$(echo "${result}" | jq -r '.correction')
-    body="**Severity:** ${severity}\n\n**Correction:** ${correction}"
+    title= $(echo "${result}" | jq -r '.severity') > $(echo "${result}" | jq -r '.title')
+    body="**Correction:** ${correction}"
     curl -X POST -H "Authorization: token $GH_TOKEN" \
         -H "Accept: application/vnd.github.v3+json" \
         https://api.github.com/repos/${GITHUB_REPOSITORY}/issues \
