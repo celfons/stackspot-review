0f56e2e78358a229a4eb4b3adddbbd21002c2caa
marcel <marcelfons@gmail.com>
2024-08-15 13:51:10 -0300

    fix

--- .github/scripts/edit_pull_request.sh
+++ .github/scripts/edit_pull_request.sh
@@ -7,14 +7,12 @@ results=$2
 # Substitui aspas simples por aspas duplas
 results=$(echo "$results" | sed "s/'/\"/g")
 
-# GitHub API URL
-API_URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${PR_NUMBER}"
-
-# Processa o JSON e atualiza a descrição do pull request no GitHub
+# Processa o JSON e cria issues no GitHub
 echo "${results}" | jq -c '.[]' | while read -r result; do
     title=$(echo "${result}" | jq -r '.title')
     body=$(echo "${result}" | jq -r '.description')
-    curl -X PATCH -H "Authorization: token ${GH_TOKEN}" -H "Accept: application/vnd.github.v3+json" \
-    ${API_URL} \
-    -d "$(jq -n --arg title "$title" --arg body "$body" '{title: $title, body: $body}')"
+    curl -X POST -H "Authorization: token $GH_TOKEN" \
+        -H "Accept: application/vnd.github.v3+json" \
+        https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${PR_NUMBER} \
+        -d "$(jq -n --arg title "$title" --arg body "$body" '{title: $title, body: $body}')"
 done
