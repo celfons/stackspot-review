e20522ac726d2a9ef205673ddf2ee5f758634ca8
Marcel Fonseca <marcel.resende@yahoo.com.br>
2024-08-15 15:49:25 -0300

    Update edit_pull_request.sh

--- .github/scripts/edit_pull_request.sh
+++ .github/scripts/edit_pull_request.sh
@@ -10,6 +10,6 @@ body=$(echo "$results" | jq -r '.description')
 
 # Realiza a requisição PATCH
 curl -X PATCH -H "Authorization: token $GH_TOKEN" \
-    -H "Accept: application/vnd.github.v3+json" \
+    -H "Accept: application/vnd.github.full+json" \
     https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${PR_NUMBER} \
     -d "$(jq -n --arg title "$title" --arg body "$body" '{title: $title, body: $body}')"  
