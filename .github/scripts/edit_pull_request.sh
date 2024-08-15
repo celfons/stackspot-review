ece8a2ad4a7af22d7fc6b9b565c6ec89baadc7d9
Marcel Fonseca <marcel.resende@yahoo.com.br>
2024-08-15 16:42:20 -0300

    Update edit_pull_request.sh

--- .github/scripts/edit_pull_request.sh
+++ .github/scripts/edit_pull_request.sh
@@ -5,7 +5,7 @@ PR_NUMBER=$1
 results=$2
 
 # Substitui aspas simples por aspas duplas
-results=$(echo "$results" | sed "s/'/\"/g")
+results=$(echo "$results" | sed "s/'//g")
 
 # Processa o JSON e atualiza a descrição do pull request no GitHub
 title=$(echo "$results" | jq -r '.title')
@@ -26,4 +26,4 @@ fi
 curl -X PATCH -H "Authorization: token $GH_TOKEN" \
     -H "Accept: application/vnd.github.full+json" \
     https://api.github.com/repos/celfons/stackspot-review/pulls/${PR_NUMBER} \
-    -d "$(jq -n --arg title "$title" --arg body "$body" '{title: $title, body: $body}')"
+    -d "{\"title\": \"$title\", \"body\": \"$body\"}"
