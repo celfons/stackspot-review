62a2058c0cb5eb3cb6ae34981534e54bfa08a1c3
Marcel Fonseca <marcel.resende@yahoo.com.br>
2024-08-15 14:53:45 -0300

    Update edit_pull_request.sh

--- .github/scripts/edit_pull_request.sh
+++ .github/scripts/edit_pull_request.sh
@@ -11,5 +11,5 @@ body=$(echo "$results" | jq -r '.description')
 curl -X PATCH \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer ${GH_TOKEN}" \
-  "${API_URL}/repos/{owner}/{repo}/pulls/${PR_NUMBER}" \
+  "${API_URL}/repos/${GITHUB_REPOSITORY}/pulls/${PR_NUMBER}" \
   -d "$(jq -n --arg title "$title" --arg body "$body" '{title: $title, body: $body}')"
