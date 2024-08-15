090ac0fbe8a30ed5ded579b0c61d05205f2b6058
Marcel Fonseca <marcel.resende@yahoo.com.br>
2024-08-15 16:22:17 -0300

    Update edit_pull_request.sh

--- .github/scripts/edit_pull_request.sh
+++ .github/scripts/edit_pull_request.sh
@@ -8,7 +8,11 @@ results=$2
 results=$(echo "$results" | sed "s/'/\"/g")
 
 # Processa o JSON e atualiza a descrição do pull request no GitHub
-title=$(echo "${results}" | jq -r '.title')
-body=$(echo "${results}" | jq -r '.description')
+title=$(echo "$results" | jq -r '.title')
+body=$(echo "$results" | jq -r '.description')
 
-curl -L \ -X PATCH \ -H "Accept: application/vnd.github+json" \ -H "Authorization: Bearer ${GH_TOKEN}" \ ${API_URL} \ -d "$(jq -n --arg title "$title" --arg body "$body" '{title: $title, body: $body}')"
+# Realiza a requisição PATCH
+curl -X PATCH -H "Authorization: token $GH_TOKEN" \
+    -H "Accept: application/vnd.github.full+json" \
+    https://api.github.com/repos/celfons/stackspot-review/pulls/${PR_NUMBER} \
+    -d "$(jq -n --arg title "$title" --arg body "$body" '{title: $title, body: $body}')"
