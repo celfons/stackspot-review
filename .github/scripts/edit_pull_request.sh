78260f0c3435b34150b9f6f752a14a2c734892db
Marcel Fonseca <marcel.resende@yahoo.com.br>
2024-08-15 16:06:44 -0300

    Update edit_pull_request.sh

--- .github/scripts/edit_pull_request.sh
+++ .github/scripts/edit_pull_request.sh
@@ -4,14 +4,15 @@
 PR_NUMBER=$1
 results=$2
 
-# Processa o JSON e atualiza a descrição do pull request no GitHub
-title=$(echo "$results" | jq -r '.title')
-body=$(echo "$results" | jq -r '.description')
+# Formata a string de entrada para JSON válido
+formatted_results=$(echo "$results" | sed "s/''/\"/g")
 
-echo "## https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${PR_NUMBER}"
+# Processa o JSON e atualiza a descrição do pull request no GitHub
+title=$(echo "$formatted_results" | jq -r '.title')
+body=$(echo "$formatted_results" | jq -r '.description')
 
 # Realiza a requisição PATCH
 curl -X PATCH -H "Authorization: token $GH_TOKEN" \
     -H "Accept: application/vnd.github.full+json" \
     https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${PR_NUMBER} \
-    -d "$(jq -n --arg title "$title" --arg body "$body" '{title: $title, body: $body}')"  
+    -d "$(jq -n --arg title "$title" --arg body "$body" '{title: $title, body: $body}')"
