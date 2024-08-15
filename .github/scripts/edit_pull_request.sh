677c74e987fe07d57dfac9f8edf12473cbb2a323
Marcel Fonseca <marcel.resende@yahoo.com.br>
2024-08-15 14:39:41 -0300

    Update edit_pull_request.sh

--- .github/scripts/edit_pull_request.sh
+++ .github/scripts/edit_pull_request.sh
@@ -8,7 +8,12 @@ results=$2
 results=$(echo "$results" | sed "s/'/\"/g")
 
 # Processa o JSON e atualiza a descrição do pull request no GitHub
-title=$(echo "${results}" | jq -r '.title')
-body=$(echo "${results}" | jq -r '.description')
+title=$(echo "$results" | jq -r '.title')
+body=$(echo "$results" | jq -r '.description')
 
-curl -L \ -X PATCH \ -H "Accept: application/vnd.github+json" \ -H "Authorization: Bearer ${GH_TOKEN}" \ ${API_URL} \ -d "$(jq -n --arg title "$title" --arg body "$body" '{title: $title, body: $body}')"
+curl -L \
+  -X PATCH \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${GH_TOKEN}" \
+  ${API_URL} \
+  -d "$(jq -n --arg title "$title" --arg body "$body" '{title: $title, body: $body}')"
