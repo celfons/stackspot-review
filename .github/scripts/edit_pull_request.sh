6210438eb85da8077c02318728381b3dcebba754
marcel <marcelfons@gmail.com>
2024-08-15 11:31:42 -0300

    adicionado teste de qc com pr

---
+++ .github/scripts/edit_pull_request.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Parameters
+PR_NUMBER=$1
+results=$2
+
+# GitHub API URL
+API_URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${PR_NUMBER}"
+
+# Substitui aspas simples por aspas duplas
+results=$(echo "$results" | sed "s/'/\"/g")
+
+# Processa o JSON e cria issues no GitHub
+echo "${results}" | jq -c '.[]' | while read -r result; do
+    description=$(echo "${result}" | jq -r '.description')
+    curl -X PATCH -H "Authorization: token ${GH_TOKEN}" -H "Accept: application/vnd.github.v3+json" \
+    ${API_URL} \
+    -d "{\"body\": \"${description}\"}"
+done
