c3ee936021654f594a3f12d396553f6125a0f16f
Marcel Fonseca <marcel.resende@yahoo.com.br>
2024-08-15 13:51:54 -0300

    Update edit_pull_request.sh

--- .github/scripts/edit_pull_request.sh
+++ .github/scripts/edit_pull_request.sh
@@ -4,18 +4,15 @@
 PR_NUMBER=$1
 results=$2
 
-results=$(echo "$results" | sed "s/'/\"/g")
-
-# GitHub API URL
-API_URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${PR_NUMBER}"
-
 # Substitui aspas simples por aspas duplas
 results=$(echo "$results" | sed "s/'/\"/g")
 
 # Processa o JSON e cria issues no GitHub
 echo "${results}" | jq -c '.[]' | while read -r result; do
-    description=$(echo "${result}" | jq -r '.description')
-    curl -X PATCH -H "Authorization: token ${GH_TOKEN}" -H "Accept: application/vnd.github.v3+json" \
-    ${API_URL} \
-    -d "{\"body\": \"${description}\"}"
+    title=$(echo "${result}" | jq -r '.title')
+    body=$(echo "${result}" | jq -r '.description')
+    curl -X POST -H "Authorization: token $GH_TOKEN" \
+        -H "Accept: application/vnd.github.v3+json" \
+        https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${PR_NUMBER} \
+        -d "$(jq -n --arg title "$title" --arg body "$body" '{title: $title, body: $body}')"
 done
